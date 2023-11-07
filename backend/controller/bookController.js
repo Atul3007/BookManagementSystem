fcd0788eed068f1b6a825a625bda1c2ade594590
atul3007 <atuldwivedi859@gmail.com>
2023-11-07 22:21:16 +0530

    index

--- backend/controller/bookController.js
+++ backend/controller/bookController.js
@@ -2,78 +2,109 @@ const { bookModel } = require("../models/bookModel");
 
 // --------get all books-------
 
-const getbookController=async(req,res)=>{
+const getbookController = async (req, res) => {
   try {
-    const books=await bookModel.find({});
+    const books = await bookModel.find({});
     res.status(200).send({
-        success: true,
-         books
-      });
+      success: true,
+      books,
+    });
   } catch (error) {
     res.status(400).send({
-        success: false,
-        error: error.message,
-        message: "Error in getting product",
-      });
+      success: false,
+      error: error.message,
+      message: "Error in getting product",
+    });
   }
-}
+};
 
 // --------create book-------
 
 const createBook = async (req, res) => {
-    try {
-      const { title, author, summary} = req.body;
-       
-      if (
-        !title ||
-        !author ||
-        !summary
-      ) {
-        return res.status(400).send({
-          message: "all fields required",
-        });
-      }
-      const book = new bookModel({ ...req.body });
-      await book.save();
-  
-      res.status(201).json({
-        success: true,
-        message: "Product created successfully",
-        book,
-      });
-    } catch (error) {
-      res.status(400).send({
-        success: false,
-        error: error.message,
-        message: "Error in creating product",
+  try {
+    const { title, author, summary } = req.body;
+
+    if (!title || !author || !summary) {
+      return res.status(400).send({
+        message: "all fields required",
       });
     }
-  };
+    const book =await new bookModel({ ...req.body });
+    await book.save();
+
+    res.status(201).json({
+      success: true,
+      message: "Product created successfully",
+      book,
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+      message: "Error in creating product",
+    });
+  }
+};
 
 //   ----------search by id -------------
 
-const getBookByid= async (req, res) => {
-    try {
-      const id = req.params.id;
-      const book = await bookModel.findById(id);
-      if (book) {
-        return res.status(201).send({
-            book
-        });
-      } else {
-        console.log("error");
-      }
-    } catch (error) {
-      res.status(400).send({
-        success: false,
-        error: error.message,
-        message: "Error in getting product photo",
+const getBookByid = async (req, res) => {
+  try {
+    const id = req.params.id;
+   // console.log(id);
+    const book = await bookModel.findById(id);
+    if (book) {
+      return res.status(201).send({
+        book,
+      });
+    } else {
+      console.log("error");
+    }
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+      message: "Error in getting product photo",
+    });
+  }
+};
+
+// -----------update book-----------
+
+const updateBook = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { title, author, summary } = req.body;
+
+    if (!title || !author || !summary) {
+      return res.status(400).send({
+        message: "all fields required",
       });
     }
-  };
-  
-module.exports={
-    getbookController,
-    createBook,
-    getBookByid
-}
+    const book = await bookModel.findByIdAndUpdate(id, {
+      ...req.body,
+    });
+    await book.save();
+
+    res.status(201).json({
+      success: true,
+      message: "Product updated successfully"
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+      message: "Error in creating product",
+    });
+  }
+};
+
+// ---------delete book---------
+
+
+module.exports = {
+  getbookController,
+  createBook,
+  getBookByid,
+  updateBook
+};
--- backend/routes/bookRoute.js
+++ backend/routes/bookRoute.js
@@ -1,11 +1,12 @@
 const express=require("express");
-const { getbookController, createBook, getBookByid } = require("../controller/bookController");
+const { getbookController, createBook, getBookByid, updateBook } = require("../controller/bookController");
 
 const bookrouter=express.Router();
 
 bookrouter.get("/all-books",getbookController);
 bookrouter.post("/create-book",createBook);
-bookrouter.get("book-id:id",getBookByid)
+bookrouter.get("/book-id/:id",getBookByid);
+bookrouter.put("/update-book/:id",updateBook)
 
 module.exports={
     bookrouter
